970f275e469f0352c300245f42504eb7bfa02b51
SubhasreeCodes <subhasreeps2001@gmail.com>
2025-01-18 12:14:23 +0530

    Update Constants.dart
    Change the IP address.

--- lib/utils/Constants.dart
+++ lib/utils/Constants.dart
@@ -1,6 +1,6 @@
 class Constants  {
 
-  static const String BASE_URL = "http://192.168.1.122:8000/api";
+  static const String BASE_URL = "http://192.168.1.4:8000/api";
 
   static const String PRODUCT_ROUTE = "/products";
   
